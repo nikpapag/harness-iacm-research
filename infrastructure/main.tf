ce42362542a0fbc04dc09053804ed7379886b0c2
nikpapag <82569427+nikpapag@users.noreply.github.com>
2025-06-28 22:36:57 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "vpc" {
   source = "app.harness.io/nPn2kgT-QbOtRRgymY-DJQ/vpc/aws"
   version = "v0.0.1"
-  name             = "${var.vpc_name}"
+  name             = "${var.name}"
   vpc_cidr         = "${var.vpc_cidr}"
   environment      = "${var.env}"
   cost_center      = "${var.cost_center}"
