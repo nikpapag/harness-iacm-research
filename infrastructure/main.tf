0657c6247271a801a932dc79681e2ecdab8ddd8b
nikpapag <82569427+nikpapag@users.noreply.github.com>
2025-06-28 22:24:48 +0100

    Create main.tf

---
+++ infrastructure/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "vpc" {
+  source = "app.harness.io/nPn2kgT-QbOtRRgymY-DJQ/vpc/aws"
+  version = "v0.0.1"
+  name             = "${var.vpc_name}"
+  vpc_cidr         = "${var.vpc_cidr}
+  environment      = "${var.env}"
+  cost_center      = "${var.cost_center}"
+  tag_owner        = "${var.tag_owner}"
+  project          = "${var.project}"
+}
