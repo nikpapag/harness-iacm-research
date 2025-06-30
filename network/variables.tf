639d85daa1c01e55195765321bf0116c267e4407
nikpapag <82569427+nikpapag@users.noreply.github.com>
2025-06-30 16:13:38 +0100

    Create variables.tf

---
+++ network/variables.tf
@@ -0,0 +1,19 @@
+variable "cost_center" {
+    description = "Cost Center for domain"
+}
+
+variable "project" {
+  description = "project identifier"
+}
+
+variable "vpc_id" {
+  description = "vpc_id"
+}
+
+variable "tag_owner" {
+    description = "Department owning the resource"
+}
+
+variable "region" {
+    description = "Department owning the resource"
+}
